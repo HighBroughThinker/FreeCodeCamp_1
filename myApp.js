7b30a179ec990f1983b35e92cb47769220e58051
WVZ-Dev <85414290+WVZ-Dev@users.noreply.github.com>
2022-04-25 19:17:04 +0200

    project 1_3

--- myApp.js
+++ myApp.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
+var absolutePath = __dirname + /views/index.html;
 
 // console.log("Hello World");
+
+// app.get('/', (req, res) => {
+//     res.send('Hello Express');
+// });
+
 app.get('/', (req, res) => {
-    res.send('Hello Express');
+    res.sendFile(absolutePath);
 });
-
-
-
 
 
 
