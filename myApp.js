b23ad39aeb6dbfd609444417417d8d6eccae3098
WVZ-Dev <85414290+WVZ-Dev@users.noreply.github.com>
2022-04-25 20:12:49 +0200

    project 1_7

--- myApp.js
+++ myApp.js
@@ -15,14 +15,10 @@ var absolutePath = __dirname + "/views/index.html";
 // app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', (req, res) => {
-    res.json({"message": "Hello json"});
-
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        response = "Hello json".toUpperCase();
-    } else {
-        response = "Hello json";
-    }
-});
+    const mySecret = process.env['MESSAGE_STYLE']
+    res.json(process.env.MESSAGE_STYLE=="uppercase" ? {"message": "HELLO JSON"}: {"message": "Hello json"})
+    });
+    
 
 
 
